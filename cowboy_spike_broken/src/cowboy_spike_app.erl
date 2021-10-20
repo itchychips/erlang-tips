2c1751849319565585e7e87df95f49ae2da673f3
Donny Johnson <itchychips@itchyeyes.net>
2021-10-19 21:18:30 -0500

    Add broken cowboy_spike and also the diff.

---
+++ cowboy_spike_broken/src/cowboy_spike_app.erl
@@ -0,0 +1,18 @@
+-module(cowboy_spike_app).
+-behaviour(application).
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    % This, commented, breaks the app.
+    %application:ensure_all_started(cowboy),
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", hello_handler, []}]}
+    ]),
+    {ok, _} = cowboy:start_clear(http,
+        [{port, 8081}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    cowboy_spike_sup:start_link().
+
+stop(_State) ->
+    ok = cowboy:stop_listener(http).
